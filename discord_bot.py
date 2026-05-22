5f92804a19356f4eb6b6c79660959f4b2fb2519f
alexisbh10 <alexisbh10@gmail.com>
2026-05-22 15:05:30 +0200

    [1.0.2] some fixes

--- discord_bot.py
+++ discord_bot.py
@@ -757,6 +757,11 @@ async def on_ready():
         guild_id = os.getenv("DISCORD_GUILD_ID")
         if guild_id:
             guild_obj = discord.Object(id=int(guild_id))
+            
+            # --- LINHA NOVA ADICIONADA AQUI ---
+            bot.tree.copy_global_to(guild=guild_obj)
+            # ----------------------------------
+            
             synced = await bot.tree.sync(guild=guild_obj)
             print(f"✅ Slash commands sincronizados en guild {guild_id}: {len(synced)}")
         else:
